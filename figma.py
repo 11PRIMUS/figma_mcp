b26509c47c888260e153f758ed5de73ba637fcb5
11PRIMUS <alokpal2803@gmail.com>
2025-04-04 01:29:45 +0530

    get images

--- figma.py
+++ figma.py
@@ -2,6 +2,7 @@ from mcp.server.fastmcp import FastMCP
 import os
 import json
 from dotenv import load_dotenv
+import requests
 
 load_dotenv()
 FIGMA_API_TOKEN=os.getenv("FIGMA_API_TOKEN")
@@ -12,10 +13,38 @@ def fetch_figma_nodes(file_key:str,node_ids:list[str])-> dict:
     headers={
         "X-Figma-Token":FIGMA_API_TOKEN
     }
-    url=f"https://api.figma.com/v1/files/{file_key}/nodes?ids={','.join(node_ids)}"
+    url=f"https://api.figma.com/v1/files/{file_key}/nodes?ids={node_ids}"
     response=requests.get(url,headers=headers)
+
     
     if response.status_code==200:
         return {"error":f"failed to fetch figma nodes: {response.status_code}"}
     
     return response.json()
+
+@mcp.tool()
+def get_node(file_key:str,node_id:str)->dict:
+    """
+    Get a node from a figma file
+    Args:
+        file_key{str}:the key of the figma file
+        node_id{str}:the id of the node
+    Returns:
+        dict- the node data if found, empty dict if not found
+    """
+    if "-" in node_id and ":" not in node_id:
+        node_id=node_id.replace("-",":")
+
+    response=fetch_figma_nodes(file_key,[node_id])
+    if "error" in response:
+        return {"error":response["error"]}
+    
+    return response
+
+
+
+file="war7ch3UIxia1m7y718uLU"
+node="10:67"
+
+print(get_node(file,node))
+
